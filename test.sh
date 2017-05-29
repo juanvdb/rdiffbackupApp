a4c32b3ecc15ee7e476d46bb7941caef0d021dc4
Juan van der Breggen <juan@breggen.net>
2017-05-29 17:31:04 +0200

    script ready to copy to rdiff

--- test.sh
+++ test.sh
@@ -16,19 +16,18 @@ LogMsg()
   if [ -z "$1" ]
   then
     # This reads a string from stdin and stores it in a variable called IN
-    read IN
+    read -r IN
   fi
 
-  local DateTime=`date "+%Y/%m/%d %H:%M:%S"`
-  echo '*****'$DateTime' ('$QMAKESPEC'): '$IN >> "$LogFile"
-  echo $DateTime' ('$QMAKESPEC'): '$IN
+  # local DateTime=`date "+%Y/%m/%d %H:%M:%S"`
+  local DateTime="["$(date "+%Y/%m/%d %H:%M:%S")"]"
+  # echo '*****'$DateTime' ('$QMAKESPEC'): '$IN >> "$LogFile"
+  echo "$DateTime: $IN" >> "$LogFile"
+  echo "$DateTime: $IN"
+  # echo $DateTime' ('$QMAKESPEC'): '$IN
 }
 
-<<<<<<< HEAD
 echo "This echo statement is piped to LogMsg" | LogMsg
 printf "This printf statement is piped to LogMsg" | LogMsg
-=======
-echo "This statement is piped to LogMsg" | LogMsg
->>>>>>> testFile
 EchoErr "This statement is an error" 2>&1 | LogMsg
 LogMsg "This statement is sent to LogMsg"
